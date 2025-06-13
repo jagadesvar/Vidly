6af43ca20bf76a64e7f106e2086cfd4e783b24a7
jagadesvar <jagadesvarr@gmail.com>
2025-06-13 18:32:07 +0530

    commit after mongodb

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const genres = require('./routes/genres.js');
 const app = express();
-const Joi = require('joi');
 app.use(express.json());
 
 app.use('/vidly/genres',genres);
 
+mongoose.connect('mongodb://localhost/vidly')
+.then(()=> console.log('Mongodb is connected...'))
+.catch((err)=> console.log('faild to connect Mongodb...'));
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>console.log(`Listning on port ${port}...`));
