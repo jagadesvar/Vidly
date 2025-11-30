2cc42ac7bfd1134490a8bbdc3c46aea512a6682e
jagadesvar <jagadesvarr@gmail.com>
2025-11-30 18:40:37 +0530

    db update

--- startup/db.js
+++ startup/db.js
@@ -1,12 +1,17 @@
-const winston = require('winston');
 const mongoose = require('mongoose');
 const config = require('config');
 
 module.exports = function () {
   const db = process.env.MONGO_URI || config.get('db');
 
+  // 👇 DEBUG LOGS – must appear in Render logs
+  console.log('DB URL from db.js =', db);
+
   mongoose
     .connect(db)
-    .then(() => winston.info(`Connected to MongoDB...`))
-    .catch(err => winston.error('Could not connect to MongoDB...', err));
+    .then(() => console.log('✅ Connected to MongoDB'))
+    .catch(err => {
+      console.error('❌ Could NOT connect to MongoDB');
+      console.error(err);
+    });
 };
